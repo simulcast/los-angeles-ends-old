1bd4f214d757197779666925d91be05713d7e781
simulcast <thisissimulcast@gmail.com>
2018-09-10 12:06:36 -0700

    fixed node.js file with port

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 
-app.listen(3000, () => console.log('listening on port 3000!'))
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
